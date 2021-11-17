cb85a640cdc61e3a5895691c5e7acac728297d93
Jedidiah <jedidiah.hwang.dev@gmail.com>
2021-11-17 09:24:23 -0700

    added post request

--- server.js
+++ server.js
@@ -19,7 +19,17 @@ app.use(express());
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
-})
+    rollbar.info("HTML file served successfully!");
+});
+
+app.post("/api/student", (req, res) => {
+    let {name} = req.body; // This needs to be let (used to be const)
+    name = name.trim();
+    students.push(name);
+
+    rollbar.log("Student added successfully", {author: "Jeddy", type: "Manual entry"})
+    res.status(200).send(students);
+});
 
 const port = process.env.PORT || 4545
 
