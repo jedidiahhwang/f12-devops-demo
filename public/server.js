7bc071108d0c973d314e624d8cd4082c6243db12
Jedidiah <jedidiah.hwang.dev@gmail.com>
2021-11-17 08:47:47 -0700

    initial commit for hosting

---
+++ public/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const Rollbar = require("rollbar");
+
+const app = express();
+
+app.use(express());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+})
+
+const port = process.env.PORT || 4545
+
+app.listen(port, () => {
+    console.log(`They're taking the Hobbits to port ${port}`);
+});
